844941bfba52f2a3f811e61f59dd64930cc82309
Kauê Soares <kaue222120@gmail.com>
2024-09-20 17:24:59 -0300

    feat: creates the project

---
+++ ame/project/src/test/java/com/challenges/backend/ame/starwars/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenges.backend.ame.starwars.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
